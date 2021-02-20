e963a5c0a8eee5e654a2472d331f4318989420a3
amirhsgh <amiqavi2601@gmail.com>
2021-02-20 15:21:35 +0330

    deploy22

--- khayyam/settings.py
+++ khayyam/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Application definition
 
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
