de66ede2b4f82e7d993a905ac617ce3f6dc7835c
amirhsgh <amiqavi2601@gmail.com>
2021-02-20 14:02:45 +0330

    deploy6

--- bot/urls.py
+++ bot/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import GetList,NumberBut
+app_name='bot'
 urlpatterns = [
     path('button',GetList.as_view(),name='button-list'),
     path('number',NumberBut.as_view(),name='number')
--- bot/views.py
+++ bot/views.py
@@ -3,8 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Button,NumberId
 import json
-
-
+import telebot
+import requests
+from telebot.types import ReplyKeyboardMarkup,KeyboardButton
+TOKEN = '1656459425:AAEymoNrG9_OP5yJh1vkXRJur4gkJoWh9-I'
+bot = telebot.TeleBot(TOKEN)
 
 class GetList(APIView):
     def get(self,request):
@@ -30,3 +33,38 @@ class NumberBut(APIView):
             return Response({"text":text.text,"code":200})
         except:
             return Response({"code":401})
+
+
+
+def Button(message):
+    r = requests.get('https://khayyam2601.herokuapp.com/api/button')
+    data = json.loads(r.text)
+    key = ReplyKeyboardMarkup(True,False)
+    text = "سلام {} شماره دانشجویی تو وارد کن :)".format(message.from_user.first_name)
+    bot.send_message(message.from_user.id,text,reply_markup=key)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    Button(message)
+
+@bot.message_handler(regexp=r'[0-9]+')
+def Check_num(message):
+    link = 'https://khayyam2601.herokuapp.com/api/number'
+    text = {"text":message.text}
+    r = requests.post(link,data=json.dumps(text))
+    data = json.loads(r.text)
+    if data['code'] == 401:
+        bot.send_message(message.from_user.id,"دسترسی غیر مجاز!!")
+    else:
+        bt = data['text']
+        bot.send_message(message.from_user.id,bt)
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://khayyam2601.herokuapp.com/' + TOKEN)
+    return "!",200
+
+
+#
+# bot.polling()
